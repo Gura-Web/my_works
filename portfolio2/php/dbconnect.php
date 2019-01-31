2e1374e5ac20ca37347a811cc17ba743e0577b58
gura0325 <>
2019-01-31 22:33:40 +0900

    ポートフォリオ２回目

--- portfolio2/php/dbconnect.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require_once("db_account.php");
-    // ローカル
-    try {  
-    $db = new PDO(
-        'mysql:dbname='.DB_NAME.';host='.DB_HOST.';charset='.CHARSET, USER_NAME, USER_PASS);
-	} catch(PDOException $e) { 
-		echo 'DB接続エラー:' . $e->getMessage();
-    }
