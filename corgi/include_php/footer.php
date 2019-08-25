18fb81fd9fccfd89a74da02c0b941f1630e478db
gura <43602198+Gura-Web@users.noreply.github.com>
2019-08-25 22:35:00 +0900

    2018-08 CORGI

---
+++ corgi/include_php/footer.php
@@ -0,0 +1,49 @@
+    <footer class="footer">
+        <div class="footer-container">
+            <nav class="footer__nav">
+                <h2 class="footer__logo"><a href="index.php"><img src="<?= IMG ?>corgi_white.svg" alt="CORGI"></a></h2>
+            <ul class="footer__col footer__col1">
+                <li class="footer__list">
+                    <ul>
+                        <li class="footer__list"><a href="about.php"><span class="footer__en">Feature</span>特徴</a></li>
+                        <li class="footer__item"><a href="#">- コーギーの種類</a></li>
+                        <li class="footer__item"><a href="#">- コーギーの見た目</a></li>
+                        <li class="footer__item"><a href="#">- 頭の良さ</a></li>
+                        <li class="footer__item"><a href="#">- 性格</a></li>
+                        <li class="footer__item"><a href="#">- 面白い特徴</a></li>
+                    </ul>
+                </li>
+            </ul>
+            <ul class="footer__col footer__col1">
+                <li class="footer__list"><a href="born.php"><span class="footer__en">Born</span>生い立ち</a>
+                    <ul>
+                        <li class="footer__item"><a href="#">- イギリス生まれ</a></li>
+                        <li class="footer__item"><a href="#">- 元牧羊犬</a></li>
+                        <li class="footer__item"><a href="#">- 個体差が大きい</a></li>
+                        <li class="footer__item"><a href="#">- 毛の色</a></li>
+                    </ul>
+                 </li>
+            </ul>
+
+            <ul class="footer__col footer__col2">
+                    <li class="footer__list"><a href="tame.php"><span class="footer__en">Tame</span>飼い方</a>
+                    <ul>
+                        <li class="footer__item"><a href="#">- 散歩時間</a></li>
+                        <li class="footer__item"><a href="#">- コーギーの食事</a></li>
+                        <li class="footer__item"><a href="#">- なりやすい病気</a></li>
+                        <li class="footer__item"><a href="#">- コーギーの寿命</a></li>
+                        <li class="footer__item"><a href="#">- コーギーの癖</a></li>
+                    </ul>
+                </li>
+            </ul>
+            <ul class="footer__col footer__col2">
+                <li class="footer__list"><a href="#"><span class="footer__en">Question</span>コーギーの疑問</a>
+                    <ul>
+                        <li class="footer__item"><a href="#">- よくある質問</a></li>
+                    </ul>
+                </li>
+            </ul>
+            </nav>
+        </div>
+    </footer>
+    <div class="goTop"><a href="#"><img src="<?= IMG ?>hand_up.png" alt=""></a></div>
